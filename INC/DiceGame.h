bde5f3783ef2feacfe8f8b999eacdff4b8ccad1d
gallon1999 <ho10230705e@gmail.com>
2025-07-26 12:39:47 +0800

    [INC] add include file

---
+++ INC/DiceGame.h
@@ -0,0 +1,18 @@
+
+#ifndef DICE_H
+#define DICE_H
+
+#include <stdbool.h>
+
+#if defined(DICE_H)
+#define _DICEGAME_DEC_ extern
+#endif
+
+#define MAX_PLAYER 10
+#define MIN_PLAYER 2
+
+
+
+_DICEGAME_DEC_ bool isAllPlayerBet( void );
+
+#endif
